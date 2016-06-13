cb81e7934cc71711f8bd9044d6c0eeb8bb3afc4a
Nicolas <nicoh22@hotmail.com>
2016-06-13 18:12:18 -0300

    Un poco de documentacion. LEER README.MD

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # orga2-tp3-2016
+
+Ideas para manejar la pantalla:
+	
+-En cada ciclo de reloj, se miran las estructuras que son
+representadas en pantalla (jugadores, tareas, relojes) y se las pinta. 
+Las funciones de "mover" solo actualizan las estructuras y limpian su 
+posicion anterior. Hace que la interrupcion de reloj sea un poco mas lenta.
+
+-Poseer una copia del pixel donde va a pararse el jugador 
+(en que estructura?), y al irse de esa posicion, restablecer el pixel copia.
+Asqueroso, pero rapido.
+
+-Las funciones que realizan movimientos se encargan de todo: pintar, 
+modificar estructuras y checkear si  en la posicion de la que me estoy yendo
+hay que dibujar alguna tarea. Esta forma implica recorrer una estructura con
+todas las tareas existentes y ver sus coordenadas.
+La interrupcion de teclado se va a atender mas lento.
+
+
+
--- tp3-bundle.v1/src/game.c
+++ tp3-bundle.v1/src/game.c
@@ -8,39 +8,47 @@
 
 jugador jugadores[2];
 
-void game_inicializar(){
+void game_inicializar(){ 
+	//inicializa estructuras del game
+	//Jugadores, puntos, virus, etc
+
 	jugadores[0].tareas_restantes = 20;
-	jugadores[0].x = 0;
+	jugadores[0].x = 1;// Totalmente arbitrario
 	jugadores[0].y = 1;
 	jugadores[0].id = 0;
 	
 	jugadores[1].tareas_restantes = 20;
-	jugadores[1].x = 79;
+	jugadores[1].x = 78;
 	jugadores[1].y = 43;
 	jugadores[1].id = 1;
 }
 
 void game_mover_cursor(int index_jugador, direccion dir) {
-	//TODO: check de limites del mapa
-	//en lugar de limpiar el pixel donde estaba,
-	//dibujar ( si corresponde ) una tarea
+	// TODO: check de limites del mapa.
+	// en lugar de limpiar el pixel donde estaba,
+	// hacer otra cosa (Referirse al Readme.md)
 	
 	jugador* jugador_actual = &jugadores[index_jugador];
 	
 	screen_limpiar_pixel(jugador_actual->x, jugador_actual->y);
 	
 	switch(dir){
-
 // La memoria de video labura asi:
 // x crece de izquierda a derecha
 // y crece de arriba hacia abajo (al reves de ejes cartesianos)
-
 		case ARB: jugadores[index_jugador].y--; break;
 		case ABA: jugadores[index_jugador].y++; break;
 		case IZQ: jugadores[index_jugador].x--; break;
 		case DER: jugadores[index_jugador].x++; break;
 	}
-	screen_pintar_jugador(jugador_actual->id, jugador_actual->x, jugador_actual->y);
+	
+	screen_pintar_jugador(
+			jugador_actual->id, 
+			jugador_actual->x, 
+			jugador_actual->y
+		);
+	
+
 }
 
 void game_lanzar(unsigned int jugador) {
